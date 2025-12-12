2b02f3d873c64d57a7db2fa73460711cd3209a33
raj2261992 <jain.rdrj@gmail.com>
2025-12-13 01:28:46 +0530

    Update hosting.py

--- mlops/hosting/hosting.py
+++ mlops/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="mlops/deployment",     # the local folder containing your files
-    repo_id="raj2261992/churn-model",          # the target repo
+    repo_id="raj2261992/Bank-Customer-Churn",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
